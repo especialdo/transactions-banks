8af2863bc4e2a33e222661b1f0919fced6a5d640
Arnaldo Rafael <arnaldo.castilla@gmail.com>
2026-03-25 18:14:22 -0500

    implementando manejo de excepciones en el microservicio

---
+++ customer/src/main/java/com/transactions/customer/domain/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.transactions.customer.domain.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
