ad3376b5ce734d8cb304b371aa52f66731258109
Arnaldo Rafael <arnaldo.castilla@gmail.com>
2026-03-27 00:23:02 -0500

    ajuste configuracion contenedor

--- customer/src/main/java/com/transactions/customer/infraestructure/config/KafkaTopicConfig.java
+++ customer/src/main/java/com/transactions/customer/infraestructure/config/KafkaTopicConfig.java
@@ -15,7 +15,7 @@ public class KafkaTopicConfig {
     @Bean
     public KafkaAdmin kafkaAdmin() {
         Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
         return new KafkaAdmin(configs);
     }
 
